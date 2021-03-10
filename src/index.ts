f0eea230e553240b657da5545a996accc2615e88
@CAYdenberg <ydenberg@gmail.com>
2021-03-10 13:12:00 -0800

    Export table cell styles

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+export * from './lib/table-cell-styles'
 export * from './mathjax'
 export * from './schema'
 export * from './transformer'
