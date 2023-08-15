66aab14927c0ecb162381e90b08f2c016e15ac3a
Mohammad Betamony <112621558+mbetamony@users.noreply.github.com>
2023-08-15 19:07:39 +0300

    LEAN-2707:  Update PM schema and create Prosemirror nodes (#36)
    * LEAN-2403: Remove bundle detection from JATS import
    
    * Revert "LEAN-2403: Remove bundle detection from JATS import"
    
    This reverts commit e661ea11a2244136bae4ad33ef2b3b1edc743d9c.
    
    * Introduce meta node, and affiliation
    
    * Add Affiliations, and some cleanups
    
    * Capitalize interface 'email'
    
    * Small fixes
    
    * Update snapshots, remove Priority from affiliation and contributor, and a fix to html.ts
    
    * Add toDom in meta_section.ts
    
    * Minor fixes
    
    * Adding email
    
    * Fixing tests
    
    * Add toDom to children of meta_section
    
    * Refactor some code
    
    * Add toDom
    
    * Removing toDom
    
    * fix version
    
    * Fix not having ObjectTypes.Contributor in node-types.ts
    
    * LEAN-2707: Publishing new version
    
    * Fix attrs of affiliation and update version
    
    ---------
    
    Co-authored-by: zax-assistant[bot] <103364958+zax-assistant[bot]@users.noreply.github.com>

---
+++ src/schema/nodes/affiliation_list.ts
@@ -0,0 +1,34 @@
+/*!
+ * © 2023 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { NodeSpec } from 'prosemirror-model'
+
+import { ManuscriptNode } from '../types'
+
+interface Attrs {
+  id: string
+}
+
+export interface AffiliationListNode extends ManuscriptNode {
+  attrs: Attrs
+}
+
+export const affiliationList: NodeSpec = {
+  content: 'affiliation*',
+  attrs: {
+    id: { default: 'AFFILIATION_LIST' },
+  },
+  toDOM: () => ['section', 0],
+}
--- src/schema/nodes/contributor.ts
+++ src/schema/nodes/contributor.ts
@@ -1,5 +1,5 @@
 /*!
- * © 2019 Atypon Systems LLC
+ * © 2023 Atypon Systems LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,44 +13,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+import { Affiliation, BibliographicName } from '@manuscripts/json-schema'
 import { NodeSpec } from 'prosemirror-model'
 
 import { ManuscriptNode } from '../types'
 
-export interface ActualManuscriptNode extends ManuscriptNode {
-  attrs: {
-    id: string
-  }
+interface Attrs {
+  id: string
+  role: string
+  affiliations: Affiliation[]
+  bibliographicName: BibliographicName
+  userID: string
+  invitationID: string
+  isCorresponding: boolean
+  ORCIDIdentifier: string
 }
 
-export const manuscript: NodeSpec = {
-  content: '(section | sections)+ meta_section', // sections is a group of elements that have group: sections
+export interface ContributorNode extends ManuscriptNode {
+  attrs: Attrs
+}
+
+export const contributor: NodeSpec = {
   attrs: {
     id: { default: '' },
-  },
-  group: 'block',
-  parseDOM: [
-    {
-      tag: 'article',
-      getAttrs: (p) => {
-        const dom = p as HTMLElement
-
-        return {
-          id: dom.getAttribute('id'),
-        }
-      },
-    },
-  ],
-  toDOM: (node) => {
-    const manuscriptNode = node as ActualManuscriptNode
-
-    return [
-      'article',
-      {
-        id: manuscriptNode.attrs.id,
-      },
-      0,
-    ]
+    role: { default: '' },
+    affiliations: { default: [] },
+    bibliographicName: { default: {} },
+    userID: { default: '' },
+    invitationID: { default: '' },
+    isCorresponding: { default: undefined },
+    ORCIDIdentifier: { default: '' },
   },
 }
