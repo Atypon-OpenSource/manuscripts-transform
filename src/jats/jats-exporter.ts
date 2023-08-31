135b9c732b7748aa7a4aa6174daea4e6fa3b57c0
Mohammad Betamony <mbetamony@atypon.com>
2023-08-31 16:11:55 +0300

    Fix test

--- src/jats/jats-exporter.ts
+++ src/jats/jats-exporter.ts
@@ -1337,15 +1337,6 @@ export class JATSExporter {
         element.appendChild(this.serializeNode(figcaptionNode))
       }
 
-      const footnotesNode = findChildNodeOfType(
-        node,
-        node.type.schema.nodes.footnotes_element
-      )
-
-      if (footnotesNode) {
-        element.appendChild(this.serializeNode(footnotesNode))
-      }
-
       node.forEach((childNode) => {
         if (childNode.type === contentNodeType) {
           if (childNode.attrs.id) {
@@ -1362,6 +1353,14 @@ export class JATSExporter {
         const attribution = this.document.createElement('attrib')
         attribution.textContent = node.attrs.attribution.literal
         element.appendChild(attribution)
+      }
+      const footnotesNode = findChildNodeOfType(
+        node,
+        node.type.schema.nodes.footnotes_element
+      )
+
+      if (footnotesNode) {
+        element.appendChild(this.serializeNode(footnotesNode))
       }
 
       if (isExecutableNodeType(node.type)) {
