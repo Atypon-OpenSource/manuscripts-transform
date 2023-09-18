e1d6e1f99c77bd88c5ed530c238e6fc7464adc47
asouqi <asouqi@atypon.com>
2023-09-18 17:21:46 +0300

    update

--- src/transformer/decode.ts
+++ src/transformer/decode.ts
@@ -211,8 +211,6 @@ export class Decoder {
     [ObjectTypes.BibliographyItem]: (data) => {
       const model = data as BibliographyItem
 
-      const commentNodes = this.createCommentsNode(model)
-      commentNodes.forEach((c) => this.comments.set(c.attrs.id, c))
       return schema.nodes.bibliography_item.create({
         id: model._id,
         type: model.type,
@@ -226,7 +224,6 @@ export class Decoder {
         page: model.page,
         title: model.title,
         literal: model.literal,
-        comments: commentNodes.map((c) => c.attrs.id),
       }) as BibliographyItemNode
     },
     [ExtraObjectTypes.PlaceholderElement]: (data) => {
