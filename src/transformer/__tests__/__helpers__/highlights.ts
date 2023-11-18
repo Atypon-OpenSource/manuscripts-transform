2fbe1b257e0e700290e23eda86423143c12ce4cc
Noura Ghazzawi <nghazzawi@atypon.com>
2023-11-18 19:24:56 +0300

    update test cases

--- src/transformer/__tests__/__helpers__/highlights.ts
+++ src/transformer/__tests__/__helpers__/highlights.ts
@@ -28,7 +28,6 @@ import {
   ParagraphElement,
   Project,
   Section,
-  Titles,
 } from '@manuscripts/json-schema'
 
 export const createTestModelMapWithHighlights = () => {
@@ -53,19 +52,6 @@ export const createTestModelMapWithHighlights = () => {
   }
 
   modelMap.set(manuscript._id, manuscript)
-
-  const titles: Titles = {
-    objectType: ObjectTypes.Titles,
-    _id: 'MPTitles:1',
-    title: 'main title',
-    subtitle: 'subtitle',
-    runningTitle: 'runningTitle',
-    createdAt: 0,
-    updatedAt: 0,
-    manuscriptID: manuscript._id,
-    containerID: project._id,
-  }
-  modelMap.set(titles._id, titles)
 
   const paragraphHighlight: Highlight = {
     objectType: ObjectTypes.Highlight,
@@ -252,19 +238,6 @@ export const createTestModelMapWithKeywords = () => {
 
     name: 'test',
   }
-
-  const titles: Titles = {
-    objectType: ObjectTypes.Titles,
-    _id: 'MPTitles:1',
-    title: 'main title',
-    subtitle: 'subtitle',
-    runningTitle: 'runningTitle',
-    createdAt: 0,
-    updatedAt: 0,
-    manuscriptID: manuscript._id,
-    containerID: project._id,
-  }
-  modelMap.set(titles._id, titles)
 
   modelMap.set(keyword._id, keyword)
 
