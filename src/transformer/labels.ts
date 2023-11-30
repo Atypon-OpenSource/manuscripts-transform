113f045d24c7aba8d5613a566ab90febf53f9d1e
Islam Momani <imomani@atypon.com>
2023-11-30 14:41:46 +0300

    Fix figure labels in the content - LEAN-2985 (#56)
    * look for graphical_abstract in every descendant node
    
    * LEAN-2985: Publishing new version
    
    * Look for graphical abstract section figures
    
    * LEAN-2985: Publishing new version
    
    * LEAN-2985: Remove prefix LEAN-2985 from package.json
    
    * LEAN-2985: Publishing new version
    
    ---------
    
    Co-authored-by: zax-assistant[bot] <103364958+zax-assistant[bot]@users.noreply.github.com>

--- src/transformer/labels.ts
+++ src/transformer/labels.ts
@@ -89,10 +89,14 @@ export const buildTargets = (
   const targets: Map<string, Target> = new Map()
   const figures: string[] = []
   fragment.forEach((node) => {
-    if (node.type === node.type.schema.nodes.graphical_abstract_section) {
-      node.forEach((item) => {
-        if (item.type === node.type.schema.nodes.figure_element) {
-          figures.push(item.attrs.id)
+    if (node.attrs.category === 'MPSectionCategory:abstracts') {
+      node.forEach((child) => {
+        if (child.type === node.type.schema.nodes.graphical_abstract_section) {
+          child.forEach((item) => {
+            if (item.type === node.type.schema.nodes.figure_element) {
+              figures.push(item.attrs.id)
+            }
+          })
         }
       })
     }
