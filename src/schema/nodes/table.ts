7fc807e0fda12167d3d92d51b73d48515d28e731
Mohammad Betamony <mbetamony@atypon.com>
2024-02-26 20:26:08 +0300

    cleanup

--- src/schema/nodes/table.ts
+++ src/schema/nodes/table.ts
@@ -14,139 +14,14 @@
  * limitations under the License.
  */
 
-import { Attrs, Node, NodeSpec } from 'prosemirror-model'
+import { NodeSpec } from 'prosemirror-model'
 import {
   TableNodes,
   tableNodes as createTableNodes,
   TableNodesOptions,
 } from 'prosemirror-tables'
 
-import {
-  getTableCellStyles,
-  serializeTableCellStyles,
-  TableCellStyleKey,
-} from '../../lib/table-cell-styles'
-
-// this is needed for the placeholder class
-const modifyTableCellSchema = (tableCell: NodeSpec) => {
-  const modifiedTableCell = { ...tableCell }
-  const originalToDOM = tableCell.toDOM
-  if (!originalToDOM) {
-    throw new Error(
-      `toDOM not found inside ${tableCell}, check for table schema changes in prosemirror-tables`
-    )
-  }
-  modifiedTableCell.toDOM = function (node) {
-    const originalArray = originalToDOM.call(this, node)
-
-    if (!Array.isArray(originalArray)) {
-      throw new Error(
-        `${originalArray} is of type ${typeof originalArray}, check for table schema changes in prosemirror-tables`
-      )
-    }
-
-    const attrsIndex = originalArray.findIndex(
-      (item) => typeof item === 'object' && !Array.isArray(item)
-    )
-
-    const modifiedAttrs = { ...originalArray[attrsIndex] }
-
-    if (!node.textContent) {
-      modifiedAttrs.class = 'placeholder'
-    }
-
-    originalArray[attrsIndex] = modifiedAttrs
-
-    return originalArray
-  }
-
-  return modifiedTableCell
-}
-
-const modifyTableToDOM = (modifiedTable: NodeSpec) => {
-  const originalToDOM = modifiedTable.toDOM
-  if (!originalToDOM) {
-    throw new Error(
-      `toDOM not found inside ${modifiedTable}, check for table schema changes in prosemirror-tables`
-    )
-  }
-  modifiedTable.toDOM = function (node: Node) {
-    const originalArray = originalToDOM.call(this, node)
-    if (!Array.isArray(originalArray)) {
-      throw new Error(
-        `${originalArray} is of type ${typeof originalArray}, check for table schema changes in prosemirror-tables`
-      )
-    }
-    const modifiedAttrs = {
-      id: node.attrs.id,
-      'data-header-rows': String(node.attrs.headerRows),
-      'data-footer-rows': String(node.attrs.footerRows),
-    }
-    // in prosemirror, the second index is the attrs
-    if (Array.isArray(originalArray[1])) {
-      originalArray.splice(1, 0, modifiedAttrs)
-    } else if (typeof originalArray[1] === 'object') {
-      originalArray[1] = { ...originalArray[1], ...modifiedAttrs }
-    } else {
-      throw new Error(
-        `toDOM[1] ${
-          originalArray[1]
-        } is of type ${typeof originalArray[1]}, check for table schema changes in prosemirror-tables`
-      )
-    }
-    return originalArray
-  }
-}
-const modifyTableParseDOM = (modifiedTable: NodeSpec) => {
-  if (!modifiedTable.parseDOM || !modifiedTable.parseDOM[0]) {
-    throw new Error(
-      `parseDOM not found inside ${modifiedTable}, check for table schema changes in prosemirror-tables`
-    )
-  }
-  const originalGetAttrs = modifiedTable.parseDOM[0].getAttrs
-  let originalAttrs: Attrs | null | boolean
-  modifiedTable.parseDOM[0].getAttrs = function (p) {
-    const dom = p as HTMLTableElement
-    if (originalGetAttrs) {
-      originalAttrs = originalGetAttrs.call(this, p)
-    }
-    if (originalAttrs && typeof originalAttrs === 'object') {
-      return {
-        ...originalAttrs,
-        id: dom.getAttribute('id'),
-        headerRows: dom.dataset && dom.dataset['header-rows'],
-        footerRows: dom.dataset && dom.dataset['footer-rows'],
-      }
-    } else {
-      return {
-        id: dom.getAttribute('id'),
-        headerRows: dom.dataset && dom.dataset['header-rows'],
-        footerRows: dom.dataset && dom.dataset['footer-rows'],
-      }
-    }
-  }
-}
-
-const modifyTableAttrs = (modifiedTable: NodeSpec, table: NodeSpec) => {
-  modifiedTable.attrs = {
-    ...table.attrs,
-    id: { default: '' },
-    dataTracked: { default: null },
-    comments: { default: null },
-    headerRows: { default: 1 },
-    footerRows: { default: 1 },
-  }
-}
-
-const modifyTableSchema = (table: NodeSpec) => {
-  const modifiedTable = { ...table }
-  modifyTableAttrs(modifiedTable, table)
-  modifyTableToDOM(modifiedTable)
-  modifyTableParseDOM(modifiedTable)
-  return modifiedTable
-}
 const tableOptions: TableNodesOptions = {
-  tableGroup: 'block',
   cellContent: 'inline*',
   cellAttributes: {
     placeholder: {
@@ -157,22 +32,6 @@ const tableOptions: TableNodesOptions = {
       setDOMAttr(value, attrs) {
         if (value) {
           attrs['data-placeholder-text'] = value
-        }
-      },
-    },
-    styles: {
-      default: {},
-      getFromDOM(dom) {
-        return getTableCellStyles(dom.style)
-      },
-      setDOMAttr(value, attrs) {
-        const styleString = serializeTableCellStyles(
-          value as {
-            [key in TableCellStyleKey]?: string | null
-          }
-        )
-        if (styleString) {
-          attrs['style'] = value
         }
       },
     },
@@ -223,9 +82,38 @@ const tableOptions: TableNodesOptions = {
   },
 }
 
-export const tableNodes: TableNodes = createTableNodes(tableOptions)
+const tableNodes: TableNodes = createTableNodes(tableOptions)
 
-export const table = modifyTableSchema(tableNodes.table)
+// this is based on prsemirror-tables schema
+export const table: NodeSpec = {
+  attrs: {
+    id: { default: '' },
+  },
+  content: 'table_row+',
+  tableRole: 'table',
+  isolating: true,
+  group: 'block',
+  parseDOM: [
+    {
+      tag: 'table',
+      getAttrs: (p) => {
+        const dom = p as HTMLElement
+        return {
+          id: dom.getAttribute('id'),
+        }
+      },
+    },
+  ],
+  toDOM(node) {
+    return [
+      'table',
+      {
+        id: node.attrs.id,
+      },
+      ['tbody', 0],
+    ]
+  },
+}
 export const tableRow = tableNodes.table_row
-export const tableCell = modifyTableCellSchema(tableNodes.table_cell)
-export const tableHeader = modifyTableCellSchema(tableNodes.table_header)
+export const tableCell = tableNodes.table_cell
+export const tableHeader = tableNodes.table_header
