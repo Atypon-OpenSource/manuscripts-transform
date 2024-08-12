808da09f26018c50444f2f0199e13b4ee27fd887
Mohammad Betamony <mbetamony@atypon.com>
2024-08-12 17:49:13 +0300

    Add CDATA to texMath

--- src/jats/jats-exporter.ts
+++ src/jats/jats-exporter.ts
@@ -1436,7 +1436,7 @@ export class JATSExporter {
       const texMath = this.document.createElement('tex-math')
       texMath.setAttribute('notation', 'LaTeX')
       texMath.setAttribute('version', 'MathJax')
-      texMath.innerHTML = node.attrs.contents
+      texMath.innerHTML = `<![CDATA[ ${node.attrs.contents} ]]>`
       return texMath
     } else {
       const math = this.nodeFromJATS(node.attrs.contents)
