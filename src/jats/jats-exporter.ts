5a38c2770c08c0b1a30dabd83988414804d39da9
Mohammad Betamony <mbetamony@atypon.com>
2024-08-26 15:26:18 +0300

    Add back CDATA

--- src/jats/jats-exporter.ts
+++ src/jats/jats-exporter.ts
@@ -1436,13 +1436,11 @@ export class JATSExporter {
       const texMath = this.document.createElement('tex-math')
       texMath.setAttribute('notation', 'LaTeX')
       texMath.setAttribute('version', 'MathJax')
-      // if (node.attrs.contents.includes('<![CDATA[')) {
-      //   texMath.innerHTML = node.attrs.contents
-      // } else {
-      //   texMath.innerHTML = `<![CDATA[ ${node.attrs.contents} ]]>`
-      // }
-      texMath.innerHTML = node.attrs.contents
-
+      if (node.attrs.contents.includes('<![CDATA[')) {
+        texMath.innerHTML = node.attrs.contents
+      } else {
+        texMath.innerHTML = `<![CDATA[ ${node.attrs.contents} ]]>`
+      }
       return texMath
     } else {
       const math = this.nodeFromJATS(node.attrs.contents)
