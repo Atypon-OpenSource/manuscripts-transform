ef495b7ed751b72cf095f0f312114ddc20031e2f
Fahid Alwaisi <82392169+fwaisi@users.noreply.github.com>
2024-08-29 15:59:37 +0300

    LEAN-3923 LW-FY25-Q2-S1: Correspondence section is showing empty when th…

--- src/transformer/decode.ts
+++ src/transformer/decode.ts
@@ -673,7 +673,6 @@ export class Decoder {
       const content = model.containedObjectIDs.map((id) =>
         this.decode(this.modelMap.get(id) as Model)
       ) as ManuscriptNode[]
-      console.log('content xx', content)
       if (content.length == 0) {
         return null
       }
