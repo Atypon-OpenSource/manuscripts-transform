a2ce4470000612330c38297aa7c36024d7ec3636
Noura Ghazzawi <nghazzawi@atypon.com>
2025-03-19 20:50:20 +0300

    LEAN-4439: Footnotes section handling

--- src/jats/importer/jats-transformations.ts
+++ src/jats/importer/jats-transformations.ts
@@ -216,19 +216,22 @@ const moveFootnotes = (
   let section = doc.querySelector('sec[sec-type="endnotes"]')
   const fnGroup =
     section?.querySelector('fn-group') || createElement('fn-group')
-
   fns.forEach((fn) => {
     if (!fn.getAttribute('fn-type')) {
       fnGroup.appendChild(fn)
     }
   })
-
   if (!section && fnGroup.innerHTML) {
     section = createFootnotesSection([fnGroup], createElement)
   }
-
   if (section) {
-    group.appendChild(section) // Appending footnotes section at the end
+    // Move the section before the last child of the group
+    const lastChild = group.lastChild
+    if (lastChild) {
+      group.insertBefore(section, lastChild)
+    } else {
+      group.appendChild(section)
+    }
   }
 }
 
