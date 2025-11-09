9b7facb1cd1dfb26d464f70b4d835f9450c3d425
Mohammad Betamony <112621558+mbetamony@users.noreply.github.com>
2025-11-09 13:29:46 +0300

    LEAN-4042 (#277)
    Co-authored-by: zax-assistant[bot] <103364958+zax-assistant[bot]@users.noreply.github.com>

--- src/jats/importer/parse-jats-article.ts
+++ src/jats/importer/parse-jats-article.ts
@@ -50,6 +50,7 @@ const processJATS = (doc: Document, sectionCategories: SectionCategory[]) => {
     return
   }
   addMissingCaptions(doc, createElement)
+  createBoxedElementSection(doc, createElement)
   createTitles(front, createElement)
   moveContributors(front, createElement)
   moveAffiliations(front, createElement)
@@ -67,7 +68,6 @@ const processJATS = (doc: Document, sectionCategories: SectionCategory[]) => {
   }
 
   moveCaptionsToEnd(body)
-  createBoxedElementSection(body, createElement)
   createBody(doc, body, createElement)
   createAbstracts(front, body, createElement)
   createBackmatter(doc, body, sectionCategories, createElement)
