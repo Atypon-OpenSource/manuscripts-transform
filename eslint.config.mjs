54c08599c46b0356896a4827e7c91c114bea70a9
asouqi <asouqi@atypon.com>
2025-11-11 02:26:56 +0300

    Upgrade eslint (LEAN-4637)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,9 +29,4 @@ const compat = new FlatCompat({
 export default defineConfig([
   ...compat.config(config),
   ...compat.extends('plugin:diff/diff'),
-  {
-    rules: {
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-    },
-  },
 ])
