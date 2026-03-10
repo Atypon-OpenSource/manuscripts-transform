4d45a6b4a8c19860a42fe3f69f0eadf40b3f51bb
Fahid Alwaisi <fwaisi@atypon.com>
2026-03-10 11:46:13 +0300

    update migration (#302)
    Co-authored-by: zax-assistant[bot] <103364958+zax-assistant[bot]@users.noreply.github.com>

--- src/schema/migration/migration-scripts/4.3.23.ts
+++ src/schema/migration/migration-scripts/4.3.23.ts
@@ -16,9 +16,9 @@
 import { JSONProsemirrorNode } from '../../../types'
 import { MigrationScript } from '../migration-script'
 
-class Migration4312 implements MigrationScript {
-  fromVersion = '4.3.11'
-  toVersion = '4.3.12'
+class Migration4323 implements MigrationScript {
+  fromVersion = '4.3.22'
+  toVersion = '4.3.23'
 
   migrateNode(node: JSONProsemirrorNode): JSONProsemirrorNode {
     if (node.type === 'contributor') {
@@ -52,4 +52,4 @@ class Migration4312 implements MigrationScript {
   }
 }
 
-export default Migration4312
+export default Migration4323
