83d359c30bf38cc29ecb2b03c2d5556688658e76
Mohammad Betamony <mbetamony@atypon.com>
2026-06-02 17:34:23 +0300

    LEAN-5625: add content-type to various elements

---
+++ src/schema/migration/migration-scripts/4.3.52.ts
@@ -0,0 +1,56 @@
+/*!
+ * © 2026 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { JSONProsemirrorNode } from '../../../types'
+import { MigrationScript } from '../migration-script'
+
+class Migration4352 implements MigrationScript {
+  fromVersion = '4.3.51'
+  toVersion = '4.3.52'
+
+  migrateNode(node: JSONProsemirrorNode): JSONProsemirrorNode {
+    switch (node.type) {
+      case 'hero_image':
+        if (!node.attrs || node.attrs.contentType === undefined) {
+          return {
+            ...node,
+            attrs: { ...(node.attrs ?? {}), contentType: 'leading' },
+          }
+        }
+        return node
+      case 'blockquote_element':
+        if (!node.attrs || node.attrs.contentType === undefined) {
+          return {
+            ...node,
+            attrs: { ...(node.attrs ?? {}), contentType: 'quote' },
+          }
+        }
+        return node
+      case 'box_element':
+      case 'table_element':
+        if (!node.attrs || node.attrs.contentType === undefined) {
+          return {
+            ...node,
+            attrs: { ...(node.attrs ?? {}), contentType: '' },
+          }
+        }
+        return node
+      default:
+        return node
+    }
+  }
+}
+
+export default Migration4352
